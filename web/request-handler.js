c525528af04bc5e68649a9c912737eb21c850e48
Hack Reactor Students <andrew@andrewzey.com>
2014-06-25 15:52:03 -0700

    Serves index page for root GET request and added sumbit button on index.html

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,24 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var httpHelpers = require('./http-helpers');
+var fs = require("fs");
+
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if(req.method === 'GET' && req.url === "/") {
+    fs.readFile(archive.paths.siteAssets + "/index.html", function (err, data) {
+      console.log('hi');
+      console.log(req.url);
+      if (err) throw err;
+      res.writeHead(200, httpHelpers.headers);
+      res.end(data);
+      console.log('hi again');
+      console.log(req.url);
+    });
+  } else {
+    res.writeHead(404, httpHelpers.headers);
+    res.end('Nice try hacker');
+  }
+  // res.end(archive.paths.list);
 };
